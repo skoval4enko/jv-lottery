8730b29b1e221722c129a3acaf11666de843e7db
Oleksandr Kovalchenko <s.koval4enko@gmail.com>
2026-03-16 18:49:49 +0200

    The task is completed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        String ballOne = lottery.getRandomBall();
+        String ballTwo = lottery.getRandomBall();
+        String ballThree = lottery.getRandomBall();
+        System.out.println(ballOne
+                + " "
+                + ballTwo
+                + " "
+                + ballThree);
     }
 }
