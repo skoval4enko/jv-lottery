56377f298509cc3bbe1b1fa06762a8969a8ac024
Oleksandr Kovalchenko <s.koval4enko@gmail.com>
2026-03-16 18:53:03 +0200

    The method was created

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(Ball.Colors.values().length);
+        Ball.Colors color = Ball.Colors.values()[index];
+        return color.toString();
     }
 }
