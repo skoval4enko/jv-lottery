973e3df16e9bdadc2143088f6f8809ccaec60411
Oleksandr Kovalchenko <s.koval4enko@gmail.com>
2026-03-17 12:16:58 +0200

    Problems were fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,13 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        String ballOne = lottery.getRandomBall();
-        String ballTwo = lottery.getRandomBall();
-        String ballThree = lottery.getRandomBall();
-        System.out.println(ballOne
-                + " "
-                + ballTwo
-                + " "
-                + ballThree);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
