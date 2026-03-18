ab03c732ddd480d09dcc3eeb0f743700801628fb
Oleksandr Kovalchenko <s.koval4enko@gmail.com>
2026-03-18 14:00:32 +0200

    Fixed the code

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
+    private static final Color[] color = Color.values();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Ball.Colors.values().length);
-        Ball.Colors color = Ball.Colors.values()[index];
-        return color.toString();
+        return color[random.nextInt(color.length)].toString();
     }
 }
