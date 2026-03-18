84da6348f7877fda9dbcbbc3f7d88f23a2a4a8c4
Oleksandr Kovalchenko <s.koval4enko@gmail.com>
2026-03-18 13:59:12 +0200

    Constant was created

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random rand = new Random();
+    private final int outOfPossibleNumber = 101;
 
     public Ball getRandomBall() {
-        int randomBall = rand.nextInt(101);
+        int randomBall = rand.nextInt(outOfPossibleNumber);
         String ballColor = colorSupplier.getRandomColor();
         return new Ball(ballColor, randomBall);
     }
