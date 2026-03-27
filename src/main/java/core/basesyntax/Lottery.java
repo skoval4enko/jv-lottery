14218c3d7de55f82c6a6195a78b5945c9453bb8b
Oleksandr Kovalchenko <s.koval4enko@gmail.com>
2026-03-27 13:44:45 +0200

    Created constant "maxNumber" instead of number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random rand = new Random();
+    private int maxNumber = 100;
 
     public Ball getRandomBall() {
-        int randomBall = rand.nextInt(100) + 1;
+        int randomBall = rand.nextInt(maxNumber) + 1;
         String ballColor = colorSupplier.getRandomColor();
         return new Ball(ballColor, randomBall);
     }
